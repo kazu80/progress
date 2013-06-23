e2e0b7c39081d2cc84e59c108a134484eb219a60
kazuyoshi.kawakami <saruyama.monki@gmail.com>
2013-06-24 00:44:49 +0900

    chapter finish
    写経完了

--- progress/progress/ViewController.m
+++ progress/progress/ViewController.m
@@ -8,11 +8,26 @@
 
 #import "ViewController.h"
 
+#define PROGRESS_SETP_TIME 0.05
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISegmentedControl *speedSegment;
+@property (weak, nonatomic) IBOutlet UISwitch *repeatSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIProgressView *timeProgress;
+@property (nonatomic)       float    progressTimer;
+@property (nonatomic)       float    performanceTime;
+@property (nonatomic)       BOOL     inProgress;
+
+- (IBAction)speedChanged:(id)sender;
+- (IBAction)touchStartButton:(id)sender;
+
 
 @end
 
 @implementation ViewController
+
+static float performanceTimes[] = {0.5, 5.0};
 
 - (void)viewDidLoad
 {
@@ -25,5 +40,58 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)initProgress
+{
+    _inProgress = NO;
+    _speedSegment.selectedSegmentIndex = 0;
+    _performanceTime = performanceTimes[_speedSegment.selectedSegmentIndex];
+    _repeatSwitch.on = NO;
+    _timeProgress.progress = 0.0;
+}
+
+- (void)startProgress
+{
+    _progressTimer = 0.0;
+    _timeProgress.progress = _progressTimer;
+    _inProgress = YES;
+    [_startButton setTitle:@"Stop" forState:UIControlStateNormal];
+    [self performSelector:@selector(updateProgress) withObject:nil afterDelay:PROGRESS_SETP_TIME];
+}
+
+- (void)stopProgress
+{
+    _inProgress = NO;
+    [_startButton setTitle:@"Start" forState:UIControlStateNormal];
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+}
+
+- (void)updateProgress
+{
+    _progressTimer += PROGRESS_SETP_TIME;
+    _timeProgress.progress = _progressTimer / _performanceTime;
+    if ( _progressTimer < _performanceTime ) {
+        [self performSelector:@selector(updateProgress) withObject:nil afterDelay:PROGRESS_SETP_TIME];
+    } else {
+        [self stopProgress];
+        if ( _repeatSwitch.on ) {
+            [self startProgress];
+        }
+    }
+}
+
+- (IBAction)speedChanged:(id)sender {
+    _performanceTime = performanceTimes[_speedSegment.selectedSegmentIndex];
+}
+
+- (IBAction)touchStartButton:(id)sender {
+    if ( _inProgress ) {
+        [self stopProgress];
+    } else {
+        [self startProgress];
+    }
+}
+
+
 
 @end
